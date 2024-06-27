f3fa34da0ae898a50a331980bf55aff27fc4f2b4
Cristiano2312 <olteanuc33@gmail.com>
2024-06-27 20:15:42 +0300

    Changes to be committed: 	new file:   Problema2.cs

---
+++ source/repos/C#/C#/Problema2.cs
@@ -0,0 +1,28 @@
+﻿
+
+using System.Reflection.Metadata.Ecma335;
+
+int[] v = { 1,2,0 };
+
+    Array.Sort(v);
+
+    for(int i=0; i<v.Length-1; i++)
+{
+    if (v[i] < 0) continue;
+
+    if (v[i] - v[i + 1] < -1)
+    {
+        Console.WriteLine(v[i] + 1);
+        break;
+    }
+    if (v[i+1] == v[v.Length-1]) 
+    {
+        Console.WriteLine(v[v.Length-1]+1);
+        break;
+    }
+}
+
+  
+
+
+
