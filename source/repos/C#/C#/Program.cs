0bd016d9743835a8850c48714a478d9041b2800a
Cristiano2312 <olteanuc33@gmail.com>
2024-06-27 18:56:31 +0300

    Initial commit Changes to be committed: 	new file:   Program.cs

---
+++ source/repos/C#/C#/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+
+    private static void Main(string[] args)
+    {
+
+        int[] array = { 3, 2, 1 };
+
+        int[] arrayNou = new int[array.Length];
+
+        int produs = 1;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            produs *= array[i];
+
+        }
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            arrayNou[i] = produs / array[i];
+            Console.WriteLine(arrayNou[i]);
+        }
+
+
+    }
+}
